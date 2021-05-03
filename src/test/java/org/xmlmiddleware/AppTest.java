c6046b77a50b26094642199b2abdfcd5cafaaa51
Carl Eric Codere <42296635+ccodere@users.noreply.github.com>
2021-05-03 17:27:59 +0800

    + Add unit test

--- src/test/java/org/xmlmiddleware/AppTest.java
+++ src/test/java/org/xmlmiddleware/AppTest.java
@@ -1,20 +1,69 @@
 package org.xmlmiddleware;
 
-import static org.junit.Assert.assertTrue;
+import java.io.InputStream;
+import java.util.Hashtable;
 
-import org.junit.Test;
+import junit.framework.TestCase;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+import org.xml.sax.InputSource;
+import org.xmlmiddleware.schemas.dtds.DTD;
+import org.xmlmiddleware.schemas.dtds.DTDParser;
+import org.xmlmiddleware.schemas.dtds.ElementType;
+import org.xmlmiddleware.xmlutils.XMLName;
+
+public class AppTest extends TestCase
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
+
+
+  protected void setUp() throws Exception
+  {
+    super.setUp();
+  }
+
+  protected void tearDown() throws Exception
+  {
+    super.tearDown();
+  }
+  
+  public void testSimpleElementTypes()
+  {
+    DTD dtd = null;
+    String nameofCurrMethod = new Exception().getStackTrace()[0].getMethodName();
+    System.out.print(nameofCurrMethod+": ");
+    
+    InputStream is = getClass().getClassLoader().getResourceAsStream("res/test.dtd");
+    
+    DTDParser dtdParser = new DTDParser();
+    
+    InputSource input = new InputSource(is);
+    try
     {
-        assertTrue( true );
+      dtd = dtdParser.parseExternalSubset(input, null, null);
+    } catch (Exception e)
+    {
+      e.printStackTrace();
+      fail();
     }
+    
+   ElementType elementType = (ElementType) dtd.elementTypes.get(XMLName.create(null,"bookList"));
+   assertNotNull(elementType);
+   Hashtable childrenElements = elementType.children;
+   ElementType bookElement = (ElementType) childrenElements.get(XMLName.create(null,"book"));
+   assertNotNull(bookElement);
+   
+   
+   ElementType author = (ElementType) bookElement.children.get(XMLName.create(null,"author"));
+   assertNotNull(author);
+   assertEquals(ElementType.CONTENT_PCDATA,author.contentType);
+   
+   ElementType title = (ElementType) bookElement.children.get(XMLName.create(null,"title"));
+   assertNotNull(title);
+   assertEquals(ElementType.CONTENT_PCDATA,title.contentType);
+   
+   ElementType id = (ElementType) bookElement.children.get(XMLName.create(null,"id"));
+   assertNotNull(id);
+   assertEquals(ElementType.CONTENT_PCDATA,id.contentType);
+   
+  }
+
 }
